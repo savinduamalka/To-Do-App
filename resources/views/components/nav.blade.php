6127c3181f3d6c41ef76aeee4ccbff76988dafec
savinduamalka <savinduamalka@gmail.com>
2024-10-23 22:14:14 +0530

    set route todo page

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -7,7 +7,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="mb-2 navbar-nav me-auto mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="{{ route('todo' )}}">ToDO List</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Link</a>
