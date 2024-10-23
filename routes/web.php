9d9d8ea52466f9f84da6f053fa9ad3a3122e69d2
savinduamalka <savinduamalka@gmail.com>
2024-10-23 09:05:48 +0530

    create route for get todo page via todo controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/",[HomeController::class,"index"])->name("home");
+Route::get("/todo",[(TodoController::class),"index"])->name("todo");
