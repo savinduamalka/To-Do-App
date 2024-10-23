3236d9c6d4d683004eebbf7ca1bbe9a1444360c7
savinduamalka <savinduamalka@gmail.com>
2024-10-23 21:45:53 +0530

    add bootsrap

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Home</h1>
-    <p>This is the home page</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+               <h1>Todo List</h1> 
+            </div>
+        </div>
+    </div>
 
 @endsection
